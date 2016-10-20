a50ddeeaf61a52013c7d07f8c8d54910ea74296c
elamre <e.vanrijnswou@innoseis.com>
2016-10-20 13:49:25 +0200

    Some code formatting

--- arduino_oct_18/arduino_oct_18.ino
+++ arduino_oct_18/arduino_oct_18.ino
@@ -1,24 +1,24 @@
 #include <Wire.h>  
-#define SLAVE_ADDRESS 0x40   // Define the i2c address
-#define trigPin 12
-#define echoPin 11
+#define SLAVE_ADDRESS               0x40   // Define the i2c address
+#define trigPin                     12
+#define echoPin                     11
 
 // [messageid][dataamount][n^data][cksum][cksum]
 
-#define PROTOCOL_BYTE_ID    0
-#define PROTOCOL_BYTE_LEN   PROTOCOL_MSG_ID
-#define PROTOCOL_BYTE_DATA  PROTOCOL_MSG_ID + PROTOCOL_MSG_LEN
+#define PROTOCOL_BYTE_ID            0
+#define PROTOCOL_BYTE_LEN           PROTOCOL_MSG_ID
+#define PROTOCOL_BYTE_DATA          PROTOCOL_MSG_ID + PROTOCOL_MSG_LEN
 
-#define PROTOCOL_MSG_ID   1
-#define PROTOCOL_MSG_LEN  1
-#define PROTOCOL_MAX_DATA 8
-#define PROTOCOL_CHCKSUM  2
-#define PROTOCOL_MAX_SIZE PROTOCOL_MAX_DATA + PROTOCOL_CHCKSUM + PROTOCOL_MSG_ID + PROTOCOL_MSG_LEN
+#define PROTOCOL_MSG_ID             1
+#define PROTOCOL_MSG_LEN            1
+#define PROTOCOL_MAX_DATA           8
+#define PROTOCOL_CHCKSUM            2
+#define PROTOCOL_MAX_SIZE           PROTOCOL_MAX_DATA + PROTOCOL_CHCKSUM + PROTOCOL_MSG_ID + PROTOCOL_MSG_LEN
 
-#define PROTOCOL_ID_RESTART 0
-#define PROTOCOL_ID_DISTANCE 1
-#define PROTOCOL_ID_LED 2
-#define PROTOCOL_ID_MOTOR 3
+#define PROTOCOL_ID_RESTART         0
+#define PROTOCOL_ID_DISTANCE        1
+#define PROTOCOL_ID_LED             2
+#define PROTOCOL_ID_MOTOR           3
 
 #define PROTOCOL_CALCULATE_LEN(data) data + PROTOCOL_CHCKSUM + PROTOCOL_MSG_ID + PROTOCOL_MSG_LEN;
 
@@ -27,23 +27,23 @@
 #define PROTOCOL_MOTOR_STRAIGHT_BIT 0x03
 #define PROTOCOL_MOTOR_STOP_BIT     0x04
 
-#define MOTOR_LEFT_PIN 6
-#define MOTOR_RIGHT_PIN 5
+#define MOTOR_LEFT_PIN              6
+#define MOTOR_RIGHT_PIN             5
 
 uint8_t sendLength = 0;
 uint8_t sendBuffer[PROTOCOL_MAX_SIZE];
 uint8_t receiveBuffer[PROTOCOL_MAX_SIZE];
 
 void requestCallback();
- int  distance;
+int  distance;
 
- int currentSpeed = 255;
+int currentSpeed = 255;
 
  void bufferWriteInt(uint8_t * buf, int value){
-  buf[0] = (value & 0xFF000000)>>24;
-  buf[1] = (value & 0x00FF0000)>>16;
-  buf[2] = (value & 0x0000FF00)>>8;
-  buf[3] = (value & 0x000000FF);
+     buf[0] = (value & 0xFF000000)>>24;
+     buf[1] = (value & 0x00FF0000)>>16;
+     buf[2] = (value & 0x0000FF00)>>8;
+     buf[3] = (value & 0x000000FF);
  }
 
  void calculateChecksum(uint8_t * protoBuffer, uint8_t dataAmount){
@@ -56,15 +56,14 @@ void requestCallback();
     protoBuffer[PROTOCOL_MSG_ID + PROTOCOL_MSG_LEN+dataAmount + 1] = (checksum& 0x00FF);
  }
  
- void setup()
- {
- Serial.begin(9600);
- Wire.begin(SLAVE_ADDRESS); 
- Wire.onRequest(requestCallback);
- pinMode(trigPin, OUTPUT);
- pinMode(echoPin, INPUT);
- pinMode(MOTOR_LEFT_PIN, OUTPUT);
- pinMode(MOTOR_RIGHT_PIN, OUTPUT);
+ void setup() {
+   Serial.begin(9600);
+   Wire.begin(SLAVE_ADDRESS); 
+   Wire.onRequest(requestCallback);
+   pinMode(trigPin, OUTPUT);
+   pinMode(echoPin, INPUT);
+   pinMode(MOTOR_LEFT_PIN, OUTPUT);
+   pinMode(MOTOR_RIGHT_PIN, OUTPUT);
 }
 
 void turnLeft(){
@@ -83,13 +82,12 @@ void turnRight(){
 }
 
 void stopDriving(){
-    analogWrite(MOTOR_LEFT_PIN, 0);
+  analogWrite(MOTOR_LEFT_PIN, 0);
   analogWrite(MOTOR_RIGHT_PIN, 0);
 }
 
-void loop() 
-{
-long duration;
+void loop(){
+  long duration;
   
   digitalWrite(trigPin, LOW);  // Added this line
   delayMicroseconds(2); // Added this line
@@ -98,34 +96,34 @@ long duration;
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
   distance = (duration/2) / 29.1;
-
-//Wire.onRequest(distance); 
- Serial.println(distance);
- delay(1000);
- testDrive();
+  
+  //Wire.onRequest(distance); 
+  Serial.println(distance);
+  delay(1000);
+  testDrive();
 }
 
 
 int state = 0;
+
 void testDrive(){
-state = (state+1) % 4;
-if(state == 0){
-  straight();
-}else if (state == 1){
-  turnLeft();
-} else if (state == 2){
-  turnRight();
-}else if (state == 3){
-  stopDriving();
-}
+  state = (state+1) % 4;
+  if(state == 0){
+    straight();
+  }else if (state == 1){
+    turnLeft();
+  } else if (state == 2){
+    turnRight();
+  }else if (state == 3){
+    stopDriving();
+  }
 }
 
 int receiveBufferIndex = 0;
 int receivedLength = 0;
 
 
-void requestCallback()
-{
+void requestCallback() {
   while(Wire.available()>1 && receivedLength != 0){
     receiveBuffer[receiveBufferIndex] = Wire.read();
     if(receiveBufferIndex == PROTOCOL_BYTE_LEN){
@@ -143,14 +141,15 @@ void requestCallback()
     case PROTOCOL_ID_RESTART:
     break;
     case PROTOCOL_ID_DISTANCE:
-      // [1][0][cksum][cksum]             -- raspberry
-      // [1][4][d][d][d][d][cksum][cksum] -- arduino
+      // [1][0][cksum][cksum]                         -- raspberry
+      // [1][4][d][d][d][d][cksum][cksum]             -- arduino
       sendBuffer[PROTOCOL_BYTE_ID] = PROTOCOL_ID_DISTANCE;
       sendLength = sendBuffer[PROTOCOL_BYTE_LEN] = PROTOCOL_CALCULATE_LEN(4);
       bufferWriteInt(&sendBuffer[PROTOCOL_MSG_ID + PROTOCOL_MSG_LEN],distance);
       calculateChecksum(sendBuffer,4);
     break;
     case PROTOCOL_ID_LED:
+    
     break;
     case PROTOCOL_ID_MOTOR:
       // [1][2][dir][speed][cksum][cksum]             -- raspberry
@@ -158,18 +157,18 @@ void requestCallback()
       uint8_t newSpeed = receiveBuffer[PROTOCOL_BYTE_DATA + 1];
       currentSpeed = newSpeed;
       if(control & PROTOCOL_MOTOR_LEFT_BIT){
-        analogWrite(MOTOR_LEFT_PIN, newSpeed);
+        analogWrite(MOTOR_LEFT_PIN,   currentSpeed);
       }
       if(control & PROTOCOL_MOTOR_RIGHT_BIT){
-        analogWrite(MOTOR_RIGHT_PIN, newSpeed);
+        analogWrite(MOTOR_RIGHT_PIN,  currentSpeed);
       }
       if(control & PROTOCOL_MOTOR_STRAIGHT_BIT){
-        analogWrite(MOTOR_LEFT_PIN, newSpeed);
-        analogWrite(MOTOR_RIGHT_PIN, newSpeed);
+        analogWrite(MOTOR_LEFT_PIN,   currentSpeed);
+        analogWrite(MOTOR_RIGHT_PIN,  currentSpeed);
       }
       if(control & PROTOCOL_MOTOR_STOP_BIT){
-        analogWrite(MOTOR_LEFT_PIN, 0);
-        analogWrite(MOTOR_RIGHT_PIN, 0);
+        analogWrite(MOTOR_LEFT_PIN,   0);
+        analogWrite(MOTOR_RIGHT_PIN,  0);
       }
     break;
   }
